94f3696002806809f1b12989bd5e89b1fa8047e2
ZCodingKid <ZCodingKid@gmail.com>
2023-03-01 20:22:06 -0500

    able to read input from textbox, will format soon

--- wordcheck.py
+++ wordcheck.py
@@ -5,13 +5,20 @@ root.geometry("800x300")
 root.configure(background="White")
 root.resizable(False, False)
 pallete = ["FAF3DD", "17183B", "8FC0A9", "3F88C5", "F18805"]
-text = Text(root, height=7, width=75, background="#FAF3DD", foreground="Black", insertbackground="Black", font="Roboto").place(x=55, y=50)
 
+# Functions
+def get_text():
+    val = text.get(1.0, "end-1c")
+    split = str(val).split(" ")
+    spaceless = [x for x in split if x != '']
+    print(len(spaceless))
 
+# Visual Feautures
+text = Text(root, background="#FAF3DD", foreground="Black", insertbackground="Black", font="Roboto", height=8, width=65)
+get_words = Button(root, text="Get Words", command=get_text).place(x=100, y=25)
+
+text.pack()
 
 words = "input('What do you want to word check: ')"
-split = words.split(" ")
-spaceless = [x for x in split if x != '']
-print(spaceless)
-print(len(spaceless))
+
 root.mainloop()
