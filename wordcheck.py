38c86f018941ecbd2b745ccf81e80480aee430ad
ZCodingKid <ZCodingKid@gmail.com>
2023-03-01 19:39:23 -0500

    setting up tkinter

--- wordcheck.py
+++ wordcheck.py
@@ -1,6 +1,12 @@
+# Initilizaions
+from tkinter import *
+root = Tk()
+root.geometry("800x300")
+root.configure(background="White")
+root.resizable(False, False)
 words = input('What do you want to word check: ')
-
 split = words.split(" ")
 spaceless = [x for x in split if x != '']
 print(spaceless)
 print(len(spaceless))
+root.mainloop()
