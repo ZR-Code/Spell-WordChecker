2b54f9f55fba6f5c17705411e0cc295855413642
ZCodingKid <ZCodingKid@gmail.com>
2023-03-03 22:40:31 -0500

    made buttons look visually appealing

--- wordcheck.py
+++ wordcheck.py
@@ -1,6 +1,8 @@
 # Initilizaions
 from tkinter import *
 from textblob import TextBlob
+import customtkinter
+
 root = Tk()
 root.title("Spell and Word Checker")
 root.geometry("800x300")
@@ -23,12 +25,9 @@ def get_text():
 scrollbar_tk = Scrollbar(root)
 scrollbar_tk.pack(side=RIGHT, fill=Y)
 text = Text(root, background="White", foreground="Black", insertbackground="Black", font="Roboto", height=8, width=65, yscrollcommand=scrollbar_tk.set)
-get_words = Button(root, text="Get Words", command=get_text, bg='#17183B').place(x=345, y=200)
+get_words = customtkinter.CTkButton(master=root, text="Get Words", command=get_text, fg_color=('#8FC0A9'), text_color = ('Black'), border_width=2, hover_color='#F18805', border_color='Black', corner_radius=20).place(x=345, y=200)
 
 text.pack(padx=5, pady=50)
 scrollbar_tk.config(command=text.yview)
-
-
-words = "input('What do you want to word check: ')"
 
 root.mainloop()
