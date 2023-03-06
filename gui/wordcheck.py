34307342666203e33a625a0672968bcfd6bc6b68
ZCodingKid <ZCodingKid@gmail.com>
2023-03-06 16:08:24 -0500

    Added original and corrected text labels

--- gui/wordcheck.py
+++ gui/wordcheck.py
@@ -12,13 +12,13 @@ subsegment_re = r'[^;:\n•]+[;,:\n•]?\s*'
 root.title("Spell and Word Checker")
 root.geometry("800x300")
 root.configure(background="#FAF3DD")
-root.resizable(False, False)
+root.resizable(True, False)
 pallete = ["FAF3DD", "17183B", "8FC0A9", "3F88C5", "F18805"]
-lblfrmetext = Frame(root, background='#FAF3DD')
 
 
 # Visual Feautures
-text = Text(lblfrmetext, background="White", foreground="Black", insertbackground="Black", font="Roboto", height=50, width=45)
+
+text = Text(root, background="White", foreground="Black", insertbackground="Black", font="Roboto", height=50, width=45)
 # Functions
 def clear_text():
 
@@ -50,6 +50,8 @@ def get_text():
                     res.append(GingerIt().parse(s)['result'])
                 fixed.append("".join(res))
     print('The Corrected sentence is: ' + ''.join(fixed))
+
+    # Scrollbar
     global lblfrmeanswer
     lblfrmeanswer = Frame(root, background='White')
     lblfrmeanswer.pack(pady=35, fill=BOTH, expand=1)
@@ -59,21 +61,21 @@ def get_text():
     scroll.pack(side=RIGHT, fill=Y)
     mycanvas.configure(yscrollcommand=scroll.set)
     mycanvas.bind('<Configure>', lambda e: mycanvas.configure(scrollregion=mycanvas.bbox("all")))
-    sec_frame = Frame(mycanvas)
+    sec_frame = Frame(mycanvas, background='White')
     mycanvas.create_window((0,0), window=sec_frame, anchor="nw")
-    og_msg = Message(sec_frame, text="The original sentence is: " + val, background='White').pack()
     correct_msg = Message(sec_frame, text="The corrected sentence is: " + ''.join(fixed), background='White').pack()
-    words_msg = Label(sec_frame, text="The amount of words in your text is: " + str(len(spaceless)), background='White').pack()
+    words_msg = Label(sec_frame, text="The amount of words in your text is: " + str(len(spaceless)) + '\n' + '\n', background='White').pack()
+    # Visuals
+    og = Label(text="Original Text", font=("Calibri", 18), background="#FAF3DD").place(x=145, y=7)
+    after = Label(text="Corrected Text and Word Amount", font=("Calibri", 18), background="#FAF3DD").place(x=500, y=7)
 
 
-    # Scrollbar
  
     print("The amount of words in the text is: " + str(len(spaceless)))
 # More Visuals
 get_words = customtkinter.CTkButton(master=root, text="Get Words", command=get_text, fg_color=('#8FC0A9'), text_color = ('Black'), border_width=1, hover_color='#F18805', border_color='Black', corner_radius=20).place(x=350, y=268)
 clear = customtkinter.CTkButton(master=root, text = "Clear Text", command=clear_text, fg_color=("#3F88C5"), text_color=("Black"), corner_radius=20, border_width=1, hover_color="#e1f222").place(x=350, y =5)
 
-lblfrmetext.pack(side=LEFT)
 text.pack(padx=5, pady=35, side=LEFT)
 
 root.mainloop()
