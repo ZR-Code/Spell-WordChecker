207c6501bec28f85f38bdf7cdf0a128121fb3a33
ZCodingKid <ZCodingKid@gmail.com>
2023-03-19 13:55:29 -0400

    Added somewhat functional tutorial screen

--- gui/enterscreen.py
+++ gui/enterscreen.py
@@ -1,7 +1,8 @@
 from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
 
-master = Tk()
-
+root = Tk()
 
 class Newscreen:
     def __init__(self, main):
@@ -11,20 +12,32 @@ class Newscreen:
         main.geometry("600x300")
         main.configure(background="#DAF1F7")
 
+        self.getstarted =Label(main, text="Get Started", font=("Comfortaa", 36), background="#DAF1F7")
+        self.getstarted.place(x=185, y=10)
 
-        self.getstarted = Label(master,text="Get Started", background="#DAF1F7", font=("Comfortaa", 36), foreground="Black").place(x=198, y =10)
-        self.tutorial = Button(master=master, text="Tutorial", background="Orange", command=self.tut).place(x=100, y=120)
-        self.jumpin = Button(master=master, text="Jump In", bg="Light Green", command=lambda : master.quit()).place(x=350, y= 120)
+        self.tutorial = ttk.Button(master=main, text="Tutorial", style="Custom.TButton", command=self.tut)
+        self.tutorial.place(x=100, y=120)
 
+        self.jumpin = ttk.Button(master=main, text="Jump In", style="C.TButton", command=lambda: main.quit())
+        self.jumpin.place(x=350, y=120)
 
+        style = ttk.Style()
+        style.theme_use('default')
+        style.configure("Custom.TButton", background="#c2f970", foreground='Black', borderwidth='1', focuscolor='none', borderradius=15)
+        style.configure("C.TButton", background="#fea82f", foreground="Black", borderwidth='1', focuscolor='none', borderradius=15)
+        
     def tut(self):
-        master.destroy()
+        self.main.destroy()
         new = Tk()
         new.geometry("600x300")
+        new.configure(background="#DAF1F7")
         new.title("Spell and Word Checker")
         new.resizable(False, False)
+        startup = Image.open("/Users/zaidr/Desktop/Coding/Word-Amount/sartupmenu.png")
+        startup_size = startup.resize((400,150))
+        startup_actual = ImageTk.PhotoImage(startup_size)
+        Label(new, image=startup_actual).place(x=100, y=50)
         new.mainloop()
 
-
-news = Newscreen(master)
-master.mainloop()
+news = Newscreen(root)
+root.mainloop()
